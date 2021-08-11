14b78bd31d3c46730d9e499854d256a0c6062911
Mike Jasiczek <mjjasiczek@mnu.edu>
2021-08-11 15:49:01 -0500

    Initial Commit for Bit Flagging

---
+++ FunWithFlags/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FunWithFlags
+{
+    class Program
+    {
+
+        public enum Flags
+        {
+            White = 1, 
+            Red = 2, 
+            Black = 4, 
+            Green = 8, 
+            Pink = 16, 
+            Hazel = 32, 
+            Yellow = 64, 
+            Orange = 128
+        }
+        static void Main(string[] args)
+        {
+
+
+            var someValues = (byte) (Flags.Orange  | Flags.Black);
+
+            byte shift = 1;
+            shift = (byte) (shift << 3);
+            
+            Console.WriteLine($"{Convert.ToString((byte)someValues, 2).PadLeft(8, '0')}");
+
+            Console.WriteLine(("------------------------------------------"));
+            Console.WriteLine($"{Convert.ToString((byte)shift, 2).PadLeft(8, '0')}");
+
+            if ((someValues & (byte) Flags.Black) == (byte) Flags.Black)
+            {
+
+                Console.WriteLine("Black was included");
+            }
+            else
+            {
+                Console.WriteLine("Black was not included");
+            }
+        }
+
+
+    }
+}
